aba21e57244e6c5428e5be5938ed62b161da880e
ToBeatElite <amitojsingh8070@gmail.com>
2022-08-28 16:42:38 -0400

    dropped linux support

--- build.sh
+++ build.sh
@@ -4,12 +4,10 @@ rm -rf bins
 mkdir bins
 rustup target add x86_64-pc-windows-gnu
 
-cargo build --release
-mv target/release/rsloader bins/rsloader
-mv target/release/rscrypter bins/rscrypter
+#cargo build --release
+#mv target/release/rsloader bins/rsloader
+#mv target/release/rscrypter bins/rscrypter
 
 cargo build --target x86_64-pc-windows-gnu --release
 mv target/x86_64-pc-windows-gnu/release/rsloader.exe bins/rsloader.exe
 mv target/x86_64-pc-windows-gnu/release/rscrypter.exe bins/rscrypter.exe
-
-strip bins/*
